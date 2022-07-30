4704efc6851c10cbcbb1a47d6261e8cf0c09ff84
ebrahemsamer2 <ebrahemsamer2@gmail.com>
2022-07-30 06:26:30 +0300

    Displaying the Number of Items in the cart in navbar
    Displaying Cart information at the top navbar

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -142,10 +142,10 @@
                 <div class="col-lg-3">
                     <div class="header__cart">
                         <ul>
-                            <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
-                            <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
+                            <li><a href="#"><i class="fa fa-heart"></i> <span>{{ $wishlist_items_count }}</span></a></li>
+                            <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>{{ $cart_items_count }}</span></a></li>
                         </ul>
-                        <div class="header__cart__price">item: <span>$150.00</span></div>
+                        <div class="header__cart__price">item: <span>${{ $total_price }}</span></div>
                     </div>
                 </div>
             </div>
